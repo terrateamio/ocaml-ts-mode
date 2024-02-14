9066a820c1c908c895307d5ec43752a6c28c75be
Malcolm Matalka <malcolm@terrateam.io>
2024-02-13 20:11:36 -0400

    Add more keywords and .mli support

--- ocaml-ts-mode.el
+++ ocaml-ts-mode.el
@@ -29,6 +29,17 @@ See `ff-other-file-alist'."
     map)
   "Keymap used in `ocaml-ts-mode'.")
 
+(defvar ocamli-ts-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-c C-a") #'ff-get-other-file)
+    map)
+  "Keymap used in `ocamli-ts-mode'.")
+
+;;;###autoload
+(defun ocaml-ts-mode-ocamlformat-before-save ()
+  (when (memq major-mode '(ocaml-ts-mode ocamli-ts-mode))
+    (ocamlformat)))
+
 (declare-function treesit-parser-create "treesit.c")
 (declare-function treesit-induce-sparse-tree "treesit.c")
 (declare-function treesit-node-start "treesit.c")
@@ -41,7 +52,6 @@ See `ff-other-file-alist'."
   :type 'integer
   :safe 'integerp
   :group 'ocaml)
-
 
 (defvar ocaml-ts-mode--syntax-table
   (let ((table (make-syntax-table)))
@@ -60,8 +70,8 @@ See `ff-other-file-alist'."
     table)
   "Syntax table for `ocaml-ts-mode'.")
 
-(defvar ocaml-ts--indent-rules
-  `((ocaml
+(defun ocaml-ts--indent-rules (language)
+  `((,language
      ((parent-is "compilation_unit") column-0 0)
      ((node-is "sig") parent-bol ocaml-ts-mode-indent-offset)
      ((node-is "struct") parent-bol ocaml-ts-mode-indent-offset)
@@ -73,15 +83,20 @@ See `ff-other-file-alist'."
   )
 
 (defvar ocaml-ts-mode--keywords
-  '(
-    "and"
+  '("and"
+    "as"
+    "assert"
+    ;; "asr"
     "begin"
     "class"
+    "constraint"
     "do"
     "done"
     "downto"
     "else"
     "end"
+    "exception"
+    "external"
     "for"
     "fun"
     "function"
@@ -89,89 +104,103 @@ See `ff-other-file-alist'."
     "if"
     "in"
     "include"
+    "inherit"
+    "initializer"
+    ;; "land"
+    "lazy"
     "let"
+    ;; "lor"
+    ;; "lsl"
+    ;; "lsr"
+    ;; "lxor"
     "match"
+    "method"
+    ;; "mod"
     "module"
     "mutable"
+    "new"
+    "nonrec"
     "object"
     "of"
     "open"
+    ;; "or"
+    "private"
     "rec"
     "sig"
     "struct"
     "then"
     "to"
+    "try"
     "type"
     "val"
+    "virtual"
     "when"
     "while"
-    "with"
-    )
+    "with")
   "Ocaml keywords for tree-sitter font-locking.")
 
-(defvar ocaml-ts-mode--font-lock-settings
+(defun ocaml-ts-mode--font-lock-settings (language)
   (treesit-font-lock-rules
-   :language 'ocaml
+   :language language
    :feature 'comment
    '((comment) @font-lock-comment-face)
 
-   :language 'ocaml
+   :language language
    :feature 'constant
    '((constructor_name) @font-lock-constant-face
      (module_name) @font-lock-type-face
      (tag) @font-lock-constant-face
      (boolean) @font-lock-constant-face)
 
-   :language 'ocaml
+   :language language
    :feature 'bracket
    '((["(" ")" "[" "]" "{" "}"]) @font-lock-bracket-face)
 
-   :language 'ocaml
+   :language language
    :feature 'delimiter
    '((["," "." ";" ":" ";;"]) @font-lock-delimiter-face)
 
-   :language 'ocaml
+   :language language
    :feature 'keyword
    `([,@ocaml-ts-mode--keywords] @font-lock-keyword-face)
 
-   :language 'ocaml
+   :language language
    :feature 'definition
    '((let_binding pattern: (value_name) @font-lock-function-name-face))
 
-   :language 'ocaml
+   :language language
    :feature 'variable
    '((value_name) @font-lock-variable-use-face
      (field_name) @font-lock-variable-use-face)
 
-   :language 'ocaml
+   :language language
    :feature 'ppx
    '((attribute_id) @font-lock-function-call-face)
 
-   :language 'ocaml
+   :language language
    :feature 'function
    :override t
    '((application_expression function: (value_path (value_name) @font-lock-function-call-face))
      (application_expression function: (value_path (module_path (_) @font-lock-type-face) (value_name) @font-lock-function-call-face)))
 
-   :language 'ocaml
+   :language language
    :feature 'number
    '((number) @font-lock-number-face)
 
-   :language 'ocaml
+   :language language
    :feature 'string
    '((string) @font-lock-string-face
      (character) @font-lock-string-face)
 
-   :language 'ocaml
+   :language language
    :feature 'escape-sequence
    :override t
    '((escape_sequence) @font-lock-escape-face)
 
-   :language 'ocaml
+   :language language
    :feature 'error
    :override t
-   '((ERROR) @font-lock-warning-face))
-  "Font-lock settings for Ocaml.")
+   '((ERROR) @font-lock-warning-face)))
 
 ;; (defun ocaml-ts-mode--defun-name (node)
 ;;   "Return the defun name of NODE.
@@ -196,11 +225,14 @@ See `ff-other-file-alist'."
   (treesit-parser-create 'ocaml)
 
   ;; Indent.
-  (setq-local treesit-simple-indent-rules ocaml-ts--indent-rules)
+  (setq-local treesit-simple-indent-rules (ocaml-ts--indent-rules 'ocaml))
 
+  (setq-local comment-start "(* ")
+  (setq-local comment-end " *)")
+  (setq-local comment-start-skip "(\\*+[ \t]*")
 
   ;; Font-lock.
-  (setq-local treesit-font-lock-settings ocaml-ts-mode--font-lock-settings)
+  (setq-local treesit-font-lock-settings (ocaml-ts-mode--font-lock-settings 'ocaml))
   (setq-local treesit-font-lock-feature-list
               '((comment number string)
                 (keyword constant)
@@ -210,11 +242,53 @@ See `ff-other-file-alist'."
   (setq ff-search-directories '(".")
         ff-other-file-alist ocaml-ts-mode-other-file-alist)
 
+  (setq-local treesit-language-at-point-function
+                  (lambda (_pos) 'ocaml))
+
+  (treesit-major-mode-setup))
+
+;;;###autoload
+(define-derived-mode ocamli-ts-mode prog-mode "Ocamli"
+  "Major mode for editing Ocaml interfaces, powered by tree-sitter."
+  :group 'ocaml
+  :syntax-table ocaml-ts-mode--syntax-table
+
+  (unless (treesit-ready-p 'ocaml-interface)
+    (error "Tree-sitter for ocaml intefface isn't available"))
+
+  (treesit-parser-create 'ocaml-interface)
+
+  ;; Indent.
+  (setq-local treesit-simple-indent-rules (ocaml-ts--indent-rules 'ocaml-interface))
+
+
+  (setq-local comment-start "(* ")
+  (setq-local comment-end " *)")
+  (setq-local comment-start-skip "(\\*+[ \t]*")
+
+  ;; Font-lock.
+  (setq-local treesit-font-lock-settings (ocaml-ts-mode--font-lock-settings 'ocaml-interface))
+  (setq-local treesit-font-lock-feature-list
+              '((comment number string)
+                (keyword constant)
+                (escape-sequence function variable definition ppx)
+                (bracket delimiter error)))
+
+  (setq ff-search-directories '(".")
+        ff-other-file-alist ocaml-ts-mode-other-file-alist)
+
+  (setq-local treesit-language-at-point-function
+                  (lambda (_pos) 'ocaml-interface))
+
   (treesit-major-mode-setup))
 
 (if (treesit-ready-p 'ocaml)
     (add-to-list 'auto-mode-alist
-                 '("\\.mli?\\'" . ocaml-ts-mode)))
+                 '("\\.ml\\'" . ocaml-ts-mode)))
+
+(if (treesit-ready-p 'ocaml-interface)
+    (add-to-list 'auto-mode-alist
+                 '("\\.mli\\'" . ocamli-ts-mode)))
 
 (provide 'ocaml-ts-mode)
 
